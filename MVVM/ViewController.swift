a0d0781a363e2f7e125b52a775e5164f1301bce9
Harshid Vaddoriya <harshidpatel388@gmail.com>
2023-07-21 15:16:01 +0530

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by HARSHID PATEL on 21/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
